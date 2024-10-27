a5e67f809ed86a582bb92a0cd7b51c0df8b9fa4c
Mohamad Mohamed <mohamad_mohamed@hotmail.de>
2024-10-27 19:54:04 +0000

    Finish crispy forms

--- blog/views.py
+++ blog/views.py
@@ -11,21 +11,22 @@ def index(request):
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
-        if request.user.is_active:
-        if request.method == "POST":
-            comment_form = CommentForm(request.POST)
+  post = get_object_or_404(Post, slug=slug)
+  if request.user.is_active:
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
 
-            if comment_form.is_valid():
-                comment = comment_form.save(commit=False)
-                comment.content_object = post
-                comment.creator = request.user
-                comment.save()
-                return redirect(request.path_info)
-        else:
-            comment_form = CommentForm()
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.content_object = post
+            comment.creator = request.user
+            comment.save()
+            return redirect(request.path_info)
     else:
-        comment_form = None
+        comment_form = CommentForm()
+  else:
+    comment_form = None
     
-    return render(
-    request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
+  return render(
+  request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
 )
